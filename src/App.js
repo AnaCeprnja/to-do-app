fb7f08ed357ddb76bf042c9507f48538eb0b654b
Ana Ceprnja <aceprnja@iun.edu>
2021-03-05 14:39:07 -0700

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+// import { Switch, Route } from 'react-router-dom'
+//import Home from './components/Home'
+import Header from './components/Header'
+//import TodosContainer from './containers/TodosContainer'
+import routes from './config/routes'
 
 function App() {
   return (
     <div className="container">
-      <h1>hello</h1>
-    </div >
+      <Header />
+      {routes}
+    </div>
   );
 }
 
